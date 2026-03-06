c9d3ac57d5336119faa856ddf4be647f6fb2887f
DOFYPXY <dofypxy@gmail.com>
2026-03-06 07:37:05 +0000

    better way to set path

--- fma/fma_tilelang.py
+++ fma/fma_tilelang.py
@@ -11,7 +11,10 @@ import tilelang
 from tilelang import Profiler
 import tilelang.language as T
 
-os.environ["PATH"] = "/usr/local/cuda-12.1/bin:" + os.environ.get("PATH", "")
+# Add CUDA to PATH if needed (use standard symlink, not version-specific)
+cuda_bin = "/usr/local/cuda/bin"
+if os.path.isdir(cuda_bin) and cuda_bin not in os.environ.get("PATH", ""):
+    os.environ["PATH"] = cuda_bin + ":" + os.environ.get("PATH", "")
 
 # Module-level cache: (N, a, b) -> compiled kernel
 _kernel_cache: dict = {}
